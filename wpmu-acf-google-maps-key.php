5542a621684d7eae39945a6da31c5cdfc8aa49cb
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-04-17 15:41:06 +0200

    Add field configuration. Place options page under general settings.

--- wpmu-acf-google-maps-key.php
+++ wpmu-acf-google-maps-key.php
@@ -17,7 +17,61 @@ class WPMUAcfGoogleMapsKey
   public function __construct()
   {
       add_action('init', [$this, 'addOptionsPage']);
+      add_action('init', [$this, 'addOptionsField']);
       add_filter('acf/fields/google_map/api', [$this, 'filterOption'], 10, 1);
+  }
+
+  public function addOptionsField() {
+    if( function_exists('acf_add_local_field_group') ):
+      acf_add_local_field_group(array(
+        'key' => 'group_643d4ad89569a',
+        'title' => 'ACF Google Maps Settings',
+        'fields' => array(
+          array(
+            'key' => 'field_643d4ad829c99',
+            'label' => 'Google Maps API Key',
+            'name' => $this->fieldOptionName,
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+          ),
+        ),
+        'location' => array(
+          array(
+            array(
+              'param' => 'options_page',
+              'operator' => '==',
+              'value' => 'acf-google-maps-key',
+            ),
+          ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'left',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+        'acfe_display_title' => '',
+        'acfe_autosync' => '',
+        'acfe_form' => 0,
+        'acfe_meta' => '',
+        'acfe_note' => '',
+      ));
+    endif;		
   }
 
   public function addOptionsPage($data)
@@ -27,7 +81,8 @@ class WPMUAcfGoogleMapsKey
       'menu_title'    => 'ACF Google Maps Key',
       'menu_slug'     => 'acf-google-maps-key',
       'capability'    => 'edit_posts',
-      'redirect'      => false
+      'redirect'      => false,
+      'parent'        => 'options-general.php'
     ));
   }
 
